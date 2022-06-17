ede436acc7c0eb453a593de029086eff51b77143
feritturgut23 <86844493+feritturgut23@users.noreply.github.com>
2022-06-17 03:54:46 +0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,4 +1,4 @@
 # -*- coding: utf-8 -*-
-TOKEN = "TOKEN YAZ"
+TOKEN = "5362523333:AAGYfnLzAtKC1lYgv6wGaUYJ0BzLgxvFNVk"
 with open('hüsnü.reqemlist.txt', 'r', encoding='utf-8') as file:
     word_list = file.read().splitlines()
