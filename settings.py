de8e34a70966f5a49b438b1c6c5eae604adb58a6
Yagiz1233dbhshs <163686002+Yagiz1233dbhshs@users.noreply.github.com>
2024-04-05 22:43:17 +0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,4 +1,4 @@
 # -*- coding: utf-8 -*-
-TOKEN = "5362523333:AAGYfnLzAtKC1lYgv6wGaUYJ0BzLgxvFNVk"
+TOKEN = "6935578968:AAHjxgnBAufpHtHHAQIBC4koKsBb_d3V4hs"
 with open('hüsnü.reqemlist.txt', 'r', encoding='utf-8') as file:
     word_list = file.read().splitlines()
